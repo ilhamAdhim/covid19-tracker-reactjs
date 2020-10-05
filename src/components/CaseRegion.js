75ea169845f43bd9a2dd3b4cded734f1af5ae087
ilhamadhim <ilhamm179@gmail.com>
2020-10-05 14:32:18 +0700

    Unbold values

--- src/components/CaseRegion.js
+++ src/components/CaseRegion.js
@@ -6,10 +6,10 @@ const CaseRegion = props => {
             <tr>
                 {'Kode_Provi' in props.region &&
                     <>
-                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Provinsi} </b> </td>
-                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Kasus_Meni} </b> </td>
-                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Kasus_Posi} </b> </td>
-                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Kasus_Semb} </b> </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> {props.region.Provinsi} </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> {props.region.Kasus_Meni} </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> {props.region.Kasus_Posi} </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> {props.region.Kasus_Semb} </td>
                     </>
 
                 }
