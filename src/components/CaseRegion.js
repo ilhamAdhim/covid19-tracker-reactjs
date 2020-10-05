b09fa4120c13b1535e2e3564acac6dc1c9666cf5
ilhamadhim <ilhamm179@gmail.com>
2020-10-05 14:23:33 +0700

    Result Province / Region

--- src/components/CaseRegion.js
+++ src/components/CaseRegion.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const CaseRegion = props => {
     return (
-        <div>
+        <>
+            <tr>
+                {'Kode_Provi' in props.region &&
+                    <>
+                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Provinsi} </b> </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Kasus_Meni} </b> </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Kasus_Posi} </b> </td>
+                        <td style={{ fontSize: "1em" }} className="text-center"> <b> {props.region.Kasus_Semb} </b> </td>
+                    </>
 
-        </div>
+                }
+            </tr>
+        </>
     );
 };
 
---
+++ src/components/TableRegion.js
@@ -0,0 +1,44 @@
+import React, { useContext, useState } from 'react';
+import CaseRegion from './CaseRegion';
+import { RegionContext } from './RegionContext'
+
+const TableRegion = props => {
+
+    // List of regions needed to be rendered
+    const [regionList, setRegionList] = useContext(RegionContext)
+    console.log(regionList);
+
+    return (
+        <>
+            <table className="table table-hover mb-4 mt-4">
+                <thead className="special-color-dark white-text text-center">
+
+                </thead>
+
+                <tbody>
+                    <tr className="text-center">
+                        <td>
+                            <b> Provinsi</b>
+                        </td>
+                        <td>
+                            <b> Meninggal</b>
+                        </td>
+                        <td>
+                            <b> Positif</b>
+                        </td>
+                        <td>
+                            <b> Sembuh </b>
+                        </td>
+                    </tr>
+                    {regionList.map(region => {
+                        return <CaseRegion key={region.Kode_Provi} region={region} />
+                    })}
+                </tbody>
+
+            </table>
+        </>
+    );
+};
+
+
+export default TableRegion;
